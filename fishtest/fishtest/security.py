649ff4b91406db74dd327b3a29a2f3dccf1fc3e4
Gary Linscott <glinscott@gmail.com>
2013-03-15 02:49:44 +0000

    Add Lucas to admins

--- fishtest/fishtest/security.py
+++ fishtest/fishtest/security.py
@@ -4,7 +4,8 @@ with open(os.path.expanduser('~/fishtest.users'), 'r') as f:
   USERS = json.load(f)
 
 GROUPS = {'glinscott': ['group:admins'],
-          'mcostalba': ['group:admins']}
+          'mcostalba': ['group:admins'],
+          'lbraesch': ['group:admins'],}
 
 def groupfinder(userid, request):
   if userid in USERS:
