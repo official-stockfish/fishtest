67900594fc3e6847551936a1519b99438dd75d4d
Marco Costalba <mcostalba@gmail.com>
2013-03-16 22:12:35 +0100

    Simplify worker_loop

--- worker/worker.py
+++ worker/worker.py
@@ -27,25 +27,17 @@ def worker_loop(worker_info, password, remote):
       try:
         req = requests.post(remote + '/api/request_version', data=json.dumps(payload))
         req = json.loads(req.text, object_hook=json_util.object_hook)
-        break
-      except:
-        sys.stderr.write('Exception accessing request_version:\n')
-        traceback.print_exc()
-        time.sleep(10)
-    else:
-      raise
 
-    if 'version' in req and int(req['version']) > WORKER_VERSION:
-      sys.stderr.write('New version available, please update your fishtest and re-run:\n')
-      break
+        if 'version' in req and int(req['version']) > WORKER_VERSION:
+           sys.stderr.write('New version available, please update your fishtest and re-run:\n')
+           return
 
-    for retry in xrange(5):
-      try:
         req = requests.post(remote + '/api/request_task', data=json.dumps(payload))
         req = json.loads(req.text, object_hook=json_util.object_hook)
         break
+
       except:
-        sys.stderr.write('Exception accessing request_task:\n')
+        sys.stderr.write('Exception accessing host:\n')
         traceback.print_exc()
         time.sleep(10)
     else:
