3acf3dd5fbbf935b034af1794ac4310f819aa3f8
Gary Linscott <glinscott@gmail.com>
2013-03-22 16:29:37 -0400

    Bump up WORKER_VERSION

--- fishtest/fishtest/api.py
+++ fishtest/fishtest/api.py
@@ -64,4 +64,4 @@ def request_version(request):
   token = authenticate(request)
   if 'error' in token: return json.dumps(token)
 
-  return json.dumps({'version': '6'})
+  return json.dumps({'version': '7'})
