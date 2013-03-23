4a3f2bc9ffc056dc24c3932b0e08d4680917b7e9
Marco Costalba <mcostalba@gmail.com>
2013-03-23 11:12:46 +0100

    Increase scavenge thresold to 60 minutes
    I often use the same net connection for my QUAD an for
    my laptop, so teh QUAD could remain off-line for a while,
    but still running games.
    
    In general we have to foreseen a short loss of net connection,
    especially in some countries.

--- fishtest/utils/scavenge.py
+++ fishtest/utils/scavenge.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 sys.path.append(os.path.expanduser('~/fishtest/fishtest'))
 from fishtest.rundb import RunDb
 
-def scavenge_tasks(scavenge=True, minutes=20):
+def scavenge_tasks(scavenge=True, minutes=60):
   """Check for tasks that have not been updated recently"""
   rundb = RunDb()
   for run in rundb.get_runs():
@@ -14,7 +14,7 @@ def scavenge_tasks(scavenge=True, minutes=20):
     for task in run['tasks']:
       if task['active'] and task['last_updated'] < datetime.utcnow() - timedelta(minutes=minutes):
         print 'Scavenging', task
-        task['active'] = False 
+        task['active'] = False
         changed = True
     if changed and scavenge:
       rundb.runs.save(run)
