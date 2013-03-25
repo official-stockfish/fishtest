031183206bf89b34a884d1c90a1eae36e3e2fcb3
Gary Linscott <glinscott@gmail.com>
2013-03-25 07:29:12 -0400

    Return version as number

--- fishtest/fishtest/api.py
+++ fishtest/fishtest/api.py
@@ -51,4 +51,4 @@ def request_version(request):
   token = authenticate(request)
   if 'error' in token: return json.dumps(token)
 
-  return json.dumps({'version': '9'})
+  return json.dumps({'version': 9})
