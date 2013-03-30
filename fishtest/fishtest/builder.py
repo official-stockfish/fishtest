80612375219ff36c10b17a1f9e24198d40cd1494
Marco Costalba <mcostalba@gmail.com>
2013-03-30 10:08:02 +0100

    Adapt to our build process

--- fishtest/fishtest/builder.py
+++ fishtest/fishtest/builder.py
@@ -1,10 +1,11 @@
 from github import Github
 from subprocess import call
 import os.path
-import urllib
+import requests
 import zipfile
 import shutil
 import time
+from zipfile import ZipFile
 
 #Parameters
 g = Github("GITHUBLOGIN", "GITHUBPASSWORD")
@@ -12,26 +13,39 @@ binaryPath="/home/ec2-user/autobuilder/binary/"
 sourcePath="/home/ec2-user/autobuilder/src/"
 repo=g.get_repo("mcostalba/Stockfish")
 
+FISHCOOKING_URL = 'https://api.github.com/repos/mcostalba/FishCooking'
+
 #To launch web server in the binary directory, type : python -c "import SimpleHTTPServer;SimpleHTTPServer.test()"
 
-def build(sha):
-    #Get the sources
-    urllib.urlretrieve ("https://github.com/mcostalba/Stockfish/archive/"+sha+".zip",sourcePath+sha+"-src.zip")
-    zfile = zipfile.ZipFile(sourcePath+sha+"-src.zip")
-    zfile.extractall(sourcePath)
-    zfile.close()
-    os.remove(sourcePath+sha+"-src.zip")
-    #Build the sources
-    os.chdir(sourcePath+"Stockfish-"+sha+"/src")
-    #Patch the makefile
-    with open("Makefile-x86_64-w64-mingw32", "wt") as out:
-        for line in open("Makefile"):
-            out.write(line.replace("CXX=g++", "CXX=x86_64-w64-mingw32-c++"))
-    shutil.copyfile("Makefile-x86_64-w64-mingw32","Makefile")
-    #Launch make
-    call(["make","build","ARCH=x86-64-modern"])
-    shutil.copyfile("stockfish",binaryPath+"stockfish-"+sha+".exe")
+def build(sha, destination, make_cmd):
+  """Download and build sources in a temporary directory then move exe to destination"""
+  cur_dir = os.cwd()
+  tmp_dir = tempfile.mkdtemp()
+  os.chdir(tmp_dir)
 
+  with open('sf.gz', 'wb+') as f:
+    f.write(requests.get(FISHCOOKING_URL + '/zipball/' + sha).content)
+  zip_file = ZipFile('sf.gz')
+  zip_file.extractall()
+  zip_file.close()
+
+  for name in zip_file.namelist():
+    if name.endswith('/src/'):
+      src_dir = name
+
+  os.chdir(src_dir)
+
+  # Patch the makefile to cross-compile
+  with open('tmp', 'wt') as out:
+    for line in open('Makefile'):
+      out.write(line.replace('CXX=g++', 'CXX=x86_64-w64-mingw32-c++'))
+    shutil.copyfile('tmp', 'Makefile')
+
+  subprocess.check_call(make_cmd, shell=True)
+
+  shutil.move('stockfish', destination)
+  os.chdir(cur_dir)
+  shutil.rmtree(tmp_dir)
 
 def survey():
     for commit in repo.get_commits()[:20]:
