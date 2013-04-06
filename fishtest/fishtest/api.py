3324aec7b41ebb1eb9572be7fb1e4bd882800dc9
Marco Costalba <mcostalba@gmail.com>
2013-04-06 18:54:38 +0200

    Bump verion 14 after retiring '-recover' option

--- fishtest/fishtest/api.py
+++ fishtest/fishtest/api.py
@@ -93,4 +93,4 @@ def request_version(request):
   token = authenticate(request)
   if 'error' in token: return json.dumps(token)
 
-  return json.dumps({'version': 13})
+  return json.dumps({'version': 14})
