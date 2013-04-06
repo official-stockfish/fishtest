5c8d08e4a67ae59b779816f2949ff7b4d17c72ce
Gary Linscott <glinscott@gmail.com>
2013-04-06 17:29:42 +0000

    Bump worker version up to 15

--- fishtest/fishtest/api.py
+++ fishtest/fishtest/api.py
@@ -93,4 +93,4 @@ def request_version(request):
   token = authenticate(request)
   if 'error' in token: return json.dumps(token)
 
-  return json.dumps({'version': 14})
+  return json.dumps({'version': 15})
--- worker/worker.py
+++ worker/worker.py
@@ -12,7 +12,7 @@ from optparse import OptionParser
 from games import run_games
 from updater import update
 
-WORKER_VERSION = 14
+WORKER_VERSION = 15
 ALIVE = True
 
 def setup_config_file(config_file):
