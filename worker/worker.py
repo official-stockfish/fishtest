8ddede95c92e7be590156a328391aa3711a4ebff
Marco Costalba <mcostalba@gmail.com>
2013-05-21 19:51:57 +0200

    Bump version to 28

--- worker/worker.py
+++ worker/worker.py
@@ -12,7 +12,7 @@ from optparse import OptionParser
 from games import run_games
 from updater import update
 
-WORKER_VERSION = 27
+WORKER_VERSION = 28
 ALIVE = True
 
 def setup_config_file(config_file):
