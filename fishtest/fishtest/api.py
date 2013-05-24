5e51ebb433ac6ac39dbcec5cdeba9ad51bcef839
Marco Costalba <mcostalba@gmail.com>
2013-05-24 10:45:33 +0200

    Bump version 29

--- fishtest/fishtest/api.py
+++ fishtest/fishtest/api.py
@@ -119,4 +119,4 @@ def request_version(request):
   token = authenticate(request)
   if 'error' in token: return json.dumps(token)
 
-  return json.dumps({'version': 28})
+  return json.dumps({'version': 29})
