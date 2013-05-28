c88fcc24ca38662fd619f955bf26c870d9a8da5d
Gary Linscott <glinscott@gmail.com>
2013-05-28 20:25:21 +0000

    Scale down # of clop processes

--- fishtest/fishtest/clop.py
+++ fishtest/fishtest/clop.py
@@ -38,7 +38,7 @@ def start_clop(run_id, branch, params):
     name = p.split('[')[0]
     minmax = p.split('[')[1].replace(',', '').split()
     s += '\nIntegerParameter %s %s %s' % (name, minmax[0], minmax[1])
-  for i in range(1, 15):
+  for i in range(1, 5):
     s += '\nProcessor %s_%d\nProcessor %s_%d' % (run_id, i, run_id, i)
   s += '\nReplications 2\nDrawElo 100\nH 3\nCorrelations all\n'
 
