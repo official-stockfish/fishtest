27a4d70fb29d68008b789d7c8e3e764ed2ce5483
Gary Linscott <glinscott@gmail.com>
2013-05-30 23:39:58 +0000

    Worker sends arguments to clop process

--- fishtest/fishtest/clop_worker/clop_worker.cpp
+++ fishtest/fishtest/clop_worker/clop_worker.cpp
@@ -10,16 +10,17 @@ int main(int argc, char** argv)
   zmq::context_t context(1);
   zmq::socket_t socket(context, ZMQ_REQ);
   socket.connect("tcp://127.0.0.1:5000");
-  while (true) {
-    zmq::message_t message(5);
-    snprintf(static_cast<char*>(message.data()), 5, "%s", argv[1]);
-    socket.send(message);
 
-    zmq::message_t request;
-    socket.recv(&request);
-    printf("Got %s\n", static_cast<char*>(request.data()));
-
-    sleep(1);
+  for (int i = 1; i < argc; ++i) {
+    int n = strlen(argv[i]);
+    zmq::message_t message(n);
+    memcpy(message.data(), argv[i], n);
+    socket.send(message, i == argc - 1 ? 0 : ZMQ_SNDMORE);
   }
+
+  zmq::message_t response;
+  socket.recv(&response);
+  printf("%s\n", static_cast<char*>(response.data()));
+
   return 0;
 }
