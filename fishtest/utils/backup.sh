e3b296adc8ca42e5d0b459b277a97961b664fa87
fishtest <fishtest@tests.stockfishchess.org>
2013-07-01 21:55:49 +0200

    Modify backup script for new host

--- fishtest/utils/backup.sh
+++ fishtest/utils/backup.sh
@@ -3,11 +3,11 @@
 source ~/.zshrc
 
 cd ~/backup
-~/mongodb-linux-x86_64-2.2.3/bin/mongodump
+~/mongodb/bin/mongodump
 rm -f dump.tar.gz
 tar cvzf dump.tar.gz dump
 
 DAY=$(date +%Y%m%d --utc -d '1 hour')
 mkdir -p archive/$DAY
 mv dump.tar.gz archive/$DAY
-s3put -b fishtest -p /home/web/ archive/$DAY/dump.tar.gz
+s3put -b fishtest -p /home/fishtest/ archive/$DAY/dump.tar.gz
