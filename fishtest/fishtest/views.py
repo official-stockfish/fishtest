0229e247e2a134ea8025e8e0433987a56604662d
Gary Linscott <glinscott@gmail.com>
2013-09-21 20:13:45 +0200

    Make sure description is set

--- fishtest/fishtest/views.py
+++ fishtest/fishtest/views.py
@@ -120,14 +120,15 @@ def actions(request):
     if action['action'] == 'modify_run':
       item['run'] = action['data']['before']['args']['new_tag']
       item['_id'] = action['data']['before']['_id']
+      item['description'] = 'modify:'
       before = action['data']['before']['args']['priority']
       after = action['data']['after']['args']['priority']
       if before != after:
-        item['description'] = 'priority changed from %s to %s' % (before, after)
+        item['description'] += ' priority changed from %s to %s' % (before, after)
       before = action['data']['before']['args']['num_games']
       after = action['data']['after']['args']['num_games']
       if before != after:
-        item['description'] = 'games changed from %s to %s' % (before, after)
+        item['description'] += 'games changed from %s to %s' % (before, after)
     else:
       item['run'] = action['data']['args']['new_tag']
       item['_id'] = action['data']['_id']
