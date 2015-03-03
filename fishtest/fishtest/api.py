d35f38fb3438557231924a09af5fe4e7d4089c37
Vince Negri <vince.negri@gmail.com>
2015-03-03 17:57:26 +0000

    Update worker version (API) as otherwise new worker will not be rolled out to existing clients and new thread option handling won't work

--- fishtest/fishtest/api.py
+++ fishtest/fishtest/api.py
@@ -142,7 +142,7 @@ def request_version(request):
   token = authenticate(request)
   if 'error' in token: return json.dumps(token)
 
-  return json.dumps({'version': 55})
+  return json.dumps({'version': 56})
 
 @view_config(route_name='api_request_spsa', renderer='string')
 def request_spsa(request):
