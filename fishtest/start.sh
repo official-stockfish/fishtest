01fe83076e853d018746946110a9e619794ff596
Gary Linscott <glinscott@gmail.com>
2015-10-18 17:17:54 +0200

    Bump up to version 57

--- fishtest/start.sh
+++ fishtest/start.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
-cd /home/fishtest/fishtest/fishtest
-
+pkill -f start_fishtest
 pkill -f pserve
+
+cd /home/fishtest/fishtest/fishtest
 nohup pserve --monitor-restart production.ini >nohup.out &
