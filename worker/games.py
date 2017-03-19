4e0de9882fde0f89d53d4ea1583da7e700f6aac6
ppigazzini <pasquale.pigazzini@gmail.com>
2017-03-19 16:44:21 +0100

    Use the standard "make" command instead of the custom "mingw32-make" command to build the applications with a Windows worker. In this way fishtest will work out of the box with all the MinGW-w64 distributions (MSYS provides also the "make" command that works fine)

--- worker/games.py
+++ worker/games.py
@@ -47,7 +47,6 @@ MAKE_CMD = 'make profile-build COMP=gcc ' + ARCH
 
 if IS_WINDOWS:
   EXE_SUFFIX = '.exe'
-  MAKE_CMD = 'mingw32-make profile-build COMP=mingw ' + ARCH
 
 def binary_filename(sha):
   system = platform.uname()[0].lower()
