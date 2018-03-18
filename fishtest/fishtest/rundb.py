c7d7a50da1c5ac2ecfe51a8456ab1639a78cba44
ppigazzini <pasquale.pigazzini@gmail.com>
2018-03-18 22:56:42 +0100

    Revert "Fix a race condition on periodic lock cleanup"
    This reverts commit a56517123249f736dc862f2d1dfdc3e7a09ae7ef.

--- fishtest/fishtest/rundb.py
+++ fishtest/fishtest/rundb.py
@@ -288,15 +288,13 @@ class RunDb:
     with self.run_lock:
       self.purge_count = self.purge_count + 1
       if self.purge_count > 100000:
-        old = time.time() - 10000
-        self.active_runs = {k: v for k, v in self.active_runs.iteritems() if v['time'] >= old}
+        self.active_runs.clear()
         self.purge_count = 0
       if id in self.active_runs:
-        active_lock = self.active_runs[id]['lock']
-        self.active_runs[id]['time'] = time.time()
+        active_lock = self.active_runs[id]
       else:
         active_lock = threading.Lock()
-        self.active_runs[id] = { 'time': time.time(), 'lock': active_lock }
+        self.active_runs[id] = active_lock
       return active_lock
 
   def update_task(self, run_id, task_id, stats, nps, spsa):
