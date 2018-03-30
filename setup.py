e3959d8dc8788c855b942350c58fec73df38d17e
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-03-30 19:29:32 +0200

    Specify test suite and fix some imports for that. Make the worker a standalone application.

--- setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-from setuptools import setup, find_packages
-
-setup(
-    name="fishtest",
-    version="0.1",
-    packages=find_packages()
-)
--- worker/test_worker.py
+++ worker/test_worker.py
@@ -1,8 +1,8 @@
 import unittest
 import worker
-import worker.worker
 import os
 import os.path
+import subprocess
 
 class workerTest(unittest.TestCase):
 
@@ -10,9 +10,8 @@ class workerTest(unittest.TestCase):
     if os.path.exists('foo.txt'):
       os.remove('foo.txt')
    
-    
   def test_config_setup(self):      
-    config = worker.worker.setup_config_file('foo.txt')
+    config = worker.setup_config_file('foo.txt')
         
     self.assertTrue(config.has_section('login'))
     self.assertTrue(config.has_section('parameters'))
@@ -21,9 +20,11 @@ class workerTest(unittest.TestCase):
     self.assertTrue(config.has_option('parameters', 'host'))
     self.assertTrue(config.has_option('parameters', 'port'))
     self.assertTrue(config.has_option('parameters', 'concurrency'))
-
-
-    
+        
+  def test_worker_script(self):
+    p = subprocess.Popen(["python" , "worker.py"], stderr = subprocess.PIPE)
+    result = p.stderr.readline()
+    self.assertEqual(result, 'worker.py [username] [password]\n')
 
 if __name__ == "__main__":
   unittest.main()
