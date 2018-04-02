eb7b962b51f83a3d83889cb8d9f11b79eb4a3cec
Tom Vijlbrief <tvijlbrief@gmail.com>
2018-04-02 10:49:53 +0200

    Add hyperlink to hardy.uhasselt.be/Toga/live_elo
    and a proper stop() to rundb.
    
    Also some minor tweaks to the Travis test.

--- test_server.py
+++ test_server.py
@@ -1,5 +1,5 @@
 import unittest
-import time
+import datetime
 
 from fishtest.fishtest.rundb import RunDb
 
@@ -10,24 +10,32 @@ class CreateServerTest(unittest.TestCase):
 
   def tearDown(self):
     # Shutdown flush thread:
-    rundb.timer = None
-    time.sleep(2)
+    rundb.stop()
 
-  def test_create_run(self):
+  def test_10_create_run(self):
     global rundb, run_id
     rundb= RunDb()
-    run_id = rundb.new_run('master', 'master', 100000, '10+0.01', 'book', 10, 1, '', '')
+    run_id = rundb.new_run('master', 'master', 100000, '10+0.01', 'book', 10, 1, '', '',
+                           username='travis', tests_repo='travis', start_time= datetime.datetime.utcnow())
     print(' '); print(run_id)
     run = rundb.get_run(run_id)
     print(run['tasks'][0])
     self.assertFalse(run['tasks'][0][u'active'])
     run['tasks'][0][u'active'] = True
     
-  def test_update_task(self):
-    r= rundb.update_task(run_id, 0, {'wins': 1, 'losses': 1, 'draws': 997}, 1000000, '')
+  def test_20_update_task(self):
+    r= rundb.update_task(run_id, 0, {'wins': 1, 'losses': 1, 'draws': 997, 'crashes': 0, 'time_losses': 0}, 1000000, '')
     self.assertEqual(r, {'task_alive': True})
-    r= rundb.update_task(run_id, 0, {'wins': 1, 'losses': 1, 'draws': 998}, 1000000, '')
+    r= rundb.update_task(run_id, 0, {'wins': 1, 'losses': 1, 'draws': 998, 'crashes': 0, 'time_losses': 0}, 1000000, '')
     self.assertEqual(r, {'task_alive': False})
+
+  def test_30_delete_run(self):
+    run = rundb.get_run(run_id)
+    run['deleted'] = True
+    run['finished'] = True
+    for w in run['tasks']:
+      w['pending'] = False
+    rundb.buffer(run, True)
 
 
 if __name__ == "__main__":
