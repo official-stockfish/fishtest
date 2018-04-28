1cf5f127baeac1a8d69f2ef8a06a167e61c56aa3
Tom Vijlbrief <tvijlbrief@gmail.com>
2018-04-28 15:42:42 +0200

    Update updater
    add requests import to updater.py
    and "rm -r requests/" 5 year old directory.
    
    Add requests dependency to setup.py

--- worker/setup.py
+++ worker/setup.py
@@ -2,9 +2,14 @@
 
 from setuptools import setup, find_packages
 
+requires = [
+    'requests'
+    ]
+
 setup(
-    name = "fishtest_worker",
-    version = "0.1",
-    packages = find_packages(),
-    test_suite = "test_worker"
+    name="fishtest_worker",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=requires,
+    test_suite="test_worker"
 )
