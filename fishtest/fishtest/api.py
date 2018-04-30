official-stockfish/fishtest

commit 8a2da7b2abb691a67e2418502d9996312a20641f

Flush print output and print time taken to fetch version and (#162)
request and update task. Increase timeouts.

fishtest/fishtest/api.py
@@ -140,7 +140,7 @@ def request_version(request):
   token = authenticate(request)
   if 'error' in token: return json.dumps(token)
 
-  return json.dumps({'version': 62})
+  return json.dumps({'version': 63})
 
 @view_config(route_name='api_request_spsa', renderer='string')
 def request_spsa(request):

worker/games.py
@@ -40,7 +40,7 @@ def is_64bit():
     return is_windows_64bit()
   return '64' in platform.architecture()[0]
 
-HTTP_TIMEOUT = 5.0
+HTTP_TIMEOUT = 15.0
 
 FISHCOOKING_URL = 'https://github.com/mcostalba/FishCooking'
 ARCH = 'ARCH=x86-64-modern' if is_64bit() else 'ARCH=x86-32'
@@ -50,6 +50,10 @@ MAKE_CMD = 'make COMP=gcc ' + ARCH
 if IS_WINDOWS:
   EXE_SUFFIX = '.exe'
   MAKE_CMD = 'make COMP=mingw ' + ARCH
+
+def printout(s):
+  print s
+  sys.stdout.flush()
 
 def github_api(repo):
   """ Convert from https://github.com/<user>/<repo>
@@ -64,7 +68,7 @@ def verify_signature(engine, signature, remote, payload, concurrency):
 
   try:
     bench_sig = ''
-    print 'Verifying signature of %s ...' % (os.path.basename(engine))
+    printout('Verifying signature of %s ...' % (os.path.basename(engine)))
     with open(os.devnull, 'wb') as f:
       p = subprocess.Popen([engine, 'bench'], stderr=subprocess.PIPE, stdout=f, universal_newlines=True)
     for line in iter(p.stderr.readline,''):
@@ -107,7 +111,7 @@ def setup(item, testing_dir):
   tree = requests.get(github_api(FISHCOOKING_URL) + '/git/trees/setup', timeout=HTTP_TIMEOUT).json()
   for blob in tree['tree']:
     if blob['path'] == item:
-      print 'Downloading %s ...' % (item)
+      printout('Downloading %s ...' % (item))
       blob_json = requests.get(blob['url'], timeout=HTTP_TIMEOUT).json()
       while not cleanup(item, testing_dir):
         blob_request = requests.get(blob['url'], timeout=HTTP_TIMEOUT)
@@ -150,7 +154,8 @@ def setup_engine(destination, worker_dir, sha, repo_url, concurrency):
         subprocess.check_call(MAKE_CMD + ' -j %s' % (concurrency) + ' strip', shell=True)
       except:
         pass
-
+      
+      
     shutil.move('stockfish'+ EXE_SUFFIX, destination)
   except:
     raise Exception('Failed to setup engine for %s' % (sha))
@@ -199,7 +204,7 @@ def adjust_tc(tc, base_nps, concurrency):
     scaled_tc = '%d/%s' % (num_moves, scaled_tc)
     tc_limit *= 100.0 / num_moves
 
-  print 'CPU factor : %f - tc adjusted to %s' % (factor, scaled_tc)
+  printout('CPU factor : %f - tc adjusted to %s' % (factor, scaled_tc))
   return scaled_tc, tc_limit
 
 def enqueue_output(out, queue):
@@ -220,7 +225,7 @@ def run_game(p, remote, result, spsa, spsa_tuning, tc_limit):
   t.start()
 
   end_time = datetime.datetime.now() + datetime.timedelta(seconds=tc_limit)
-  print('TC limit', tc_limit, 'End time:', end_time)
+  printout('TC limit '+str(tc_limit)+' End time: '+str(end_time))
 
   while datetime.datetime.now() < end_time:
     try: line = q.get_nowait()
@@ -235,7 +240,7 @@ def run_game(p, remote, result, spsa, spsa_tuning, tc_limit):
 
     # Have we reached the end of the match?  Then just exit
     if 'Finished match' in line:
-      print('Finished match cleanly')
+      printout('Finished match cleanly')
       kill_process(p)
       break
 
@@ -263,12 +268,14 @@ def run_game(p, remote, result, spsa, spsa_tuning, tc_limit):
         spsa['draws'] = wld[2]
 
       try:
+        t0 = datetime.datetime.utcnow()
         req = requests.post(remote + '/api/update_task', data=json.dumps(result), headers={'Content-type': 'application/json'}, timeout=HTTP_TIMEOUT).json()
         failed_updates = 0
+        printout("Task updated successfully in "+str((datetime.datetime.utcnow()-t0).total_seconds())+"s")
 
         if not req['task_alive']:
           # This task is no longer neccesary
-          print('Server told us task is no longer needed')
+          printout('Server told us task is no longer needed')
           kill_process(p)
           return req
 
@@ -277,13 +284,13 @@ def run_game(p, remote, result, spsa, spsa_tuning, tc_limit):
         traceback.print_exc(file=sys.stderr)
         failed_updates += 1
         if failed_updates > 5:
-          print('Too many failed update attempts')
+          printout('Too many failed update attempts')
           kill_process(p)
           break
         time.sleep(random.randint(failed_updates*failed_updates, 4 * failed_updates*failed_updates))
 
   if datetime.datetime.now() >= end_time:
-    print(datetime.datetime.now(), 'is past end time', end_time)
+    printout(datetime.datetime.now()+' is past end time '+end_time)
     kill_process(p)
 
   return { 'task_alive': True }
@@ -314,7 +321,7 @@ def launch_cutechess(cmd, remote, result, spsa_tuning, games_to_play, tc_limit):
   idx = cmd.index('_spsa_')
   cmd = cmd[:idx] + ['option.%s=%d'%(x['name'], round(x['value'])) for x in b_params] + cmd[idx+1:]
 
-  print cmd
+  printout(cmd)
   p = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True, bufsize=1, close_fds=not IS_WINDOWS)
 
   try:
@@ -322,7 +329,7 @@ def launch_cutechess(cmd, remote, result, spsa_tuning, games_to_play, tc_limit):
   except:
     traceback.print_exc(file=sys.stderr)
     try:
-      print('Exception running games')
+      printout('Exception running games')
       kill_process(p)
     except:
       pass
@@ -442,7 +449,7 @@ def run_games(worker_info, password, remote, run, task_id):
   else:
     book_cmd = ['book=%s' % (book), 'bookdepth=%s' % (book_depth)]
 
-  print 'Running %s vs %s' % (run['args']['new_tag'], run['args']['base_tag'])
+  printout('Running %s vs %s' % (run['args']['new_tag'], run['args']['base_tag']))
 
   if spsa_tuning:
     games_to_play = games_concurrency * 2

worker/worker.py
@@ -14,11 +14,16 @@ from ConfigParser import SafeConfigParser
 from optparse import OptionParser
 from games import run_games
 from updater import update
+from datetime import datetime
 
-WORKER_VERSION = 62
+WORKER_VERSION = 63
 ALIVE = True
 
-HTTP_TIMEOUT = 5.0
+HTTP_TIMEOUT = 15.0
+
+def printout(s):
+  print s
+  sys.stdout.flush()
 
 def setup_config_file(config_file):
   ''' Config file setup, adds defaults if not existing '''
@@ -56,6 +61,7 @@ def worker(worker_info, password, remote):
   try:
     print 'Will fetch task soon...'
     time.sleep(random.randint(1,30))
+    t0 = datetime.utcnow()
     req = requests.post(remote + '/api/request_version', data=json.dumps(payload), headers={'Content-type': 'application/json'}, timeout=HTTP_TIMEOUT)
     req = json.loads(req.text)
 
@@ -65,9 +71,11 @@ def worker(worker_info, password, remote):
       sys.exit(1)
 
     if req['version'] > WORKER_VERSION:
-      print 'Updating worker version to %d' % (req['version'])
+      printout('Updating worker version to %d' % (req['version']))
       update()
+    printout("Worker version checked successfully in "+str((datetime.utcnow()-t0).total_seconds())+"s")
 
+    t0 = datetime.utcnow()
     req = requests.post(remote + '/api/request_task', data=json.dumps(payload), headers={'Content-type': 'application/json'}, timeout=HTTP_TIMEOUT)
     req = json.loads(req.text)
   except:
@@ -76,12 +84,13 @@ def worker(worker_info, password, remote):
     time.sleep(random.randint(10,60))
     return
 
+  printout("Task requested in "+str((datetime.utcnow()-t0).total_seconds())+"s")
   if 'error' in req:
     raise Exception('Error from remote: %s' % (req['error']))
 
   # No tasks ready for us yet, just wait...
   if 'task_waiting' in req:
-    print 'No tasks available at this time, waiting...'
+    printout('No tasks available at this time, waiting...\n')
     time.sleep(random.randint(10,60))
     return
 
@@ -109,6 +118,7 @@ def worker(worker_info, password, remote):
   return success
 
 def main():
+  printout("Worker starting ...\n")
   signal.signal(signal.SIGINT, on_sigint)
   signal.signal(signal.SIGTERM, on_sigint)
 
@@ -144,7 +154,7 @@ def main():
     config.write(f)
 
   remote = 'http://%s:%s' % (options.host, options.port)
-  print 'Worker version %d connecting to %s' % (WORKER_VERSION, remote)
+  printout('Worker version %d connecting to %s' % (WORKER_VERSION, remote))
 
   try:
     cpu_count = min(int(options.concurrency), multiprocessing.cpu_count() - 1)

commit a9db4e32f542e71173e555231e9e07c34cd9fec0

Fetch unique_key from task
A small optimization.

In addition add a lock to get_worker_key()
because it has a race condition.

Fix print issue introduced in #162

https://github.com/glinscott/fishtest/commit/8a2da7b2abb691a67e2418502d9996312a20641f

fishtest/fishtest/api.py
@@ -96,7 +96,6 @@ def update_task(request):
   if 'error' in token: return json.dumps(token)
 
   result = request.rundb.update_task(
-    worker=request.json_body['unique_key'],
     run_id=request.json_body['run_id'],
     task_id=int(request.json_body['task_id']),
     stats=request.json_body['stats'],
@@ -150,4 +149,4 @@ def request_spsa(request):
   run_id = request.json_body['run_id']
   task_id = int(request.json_body['task_id'])
 
-  return json.dumps(request.rundb.request_spsa(request.json_body['unique_key'], run_id, task_id))
+  return json.dumps(request.rundb.request_spsa(run_id, task_id))

fishtest/fishtest/rundb.py
@@ -343,12 +343,12 @@ class RunDb:
         self.active_runs[id] = { 'time': time.time(), 'lock': active_lock }
       return active_lock
 
-  def update_task(self, worker, run_id, task_id, stats, nps, spsa, username):
+  def update_task(self, run_id, task_id, stats, nps, spsa, username):
     lock = self.active_run_lock(str(run_id))
     with lock:
-      return self.sync_update_task(worker, run_id, task_id, stats, nps, spsa, username)
+      return self.sync_update_task(run_id, task_id, stats, nps, spsa, username)
 
-  def sync_update_task(self, worker, run_id, task_id, stats, nps, spsa, username):
+  def sync_update_task(self, run_id, task_id, stats, nps, spsa, username):
 
     run = self.get_run(run_id)
     if task_id >= len(run['tasks']):
@@ -386,7 +386,7 @@ class RunDb:
 
     # Update spsa results
     if 'spsa' in run['args'] and spsa_games == spsa['num_games']:
-      self.update_spsa(worker, run, spsa)
+      self.update_spsa(task['worker_info']['unique_key'], run, spsa)
 
     # Check if SPRT stopping is enabled
     if 'sprt' in run['args']:
@@ -495,7 +495,7 @@ class RunDb:
     run_id = str(run_id)
     if not run_id in self.spsa_params:
       # Should only happen after server restart
-      return self.generate_spsa(worker, self.get_run(run_id))['w_params']
+      return self.generate_spsa(self.get_run(run_id))['w_params']
     return self.spsa_params[run_id][worker]
 
   def clear_params(self, run_id):
@@ -503,7 +503,7 @@ class RunDb:
     if run_id in self.spsa_params:
       del self.spsa_params[run_id]
 
-  def request_spsa(self, worker, run_id, task_id):
+  def request_spsa(self, run_id, task_id):
     run = self.get_run(run_id)
 
     if task_id >= len(run['tasks']):
@@ -512,11 +512,11 @@ class RunDb:
     if not task['active'] or not task['pending']:
       return {'task_alive': False}
 
-    result = self.generate_spsa(worker, run)
-    self.store_params(run['_id'], worker, result['w_params'])
+    result = self.generate_spsa(run)
+    self.store_params(run['_id'], task['worker_info']['unique_key'], result['w_params'])
     return result
 
-  def generate_spsa(self, worker, run):
+  def generate_spsa(self, run):
     result = {
       'task_alive': True,
       'w_params': [],

fishtest/fishtest/views.py
@@ -646,6 +646,8 @@ def format_results(run_results, run):
   return result
 
 UUID_MAP = defaultdict(dict)
+key_lock = threading.Lock()
+
 def get_worker_key(task):
   global UUID_MAP
 
@@ -655,9 +657,10 @@ def get_worker_key(task):
   cores = str(task['worker_info']['concurrency'])
 
   uuid = task['worker_info'].get('unique_key', '')
-  if uuid not in UUID_MAP[username]:
-    next_idx = len(UUID_MAP[username])
-    UUID_MAP[username][uuid] = next_idx
+  with key_lock:
+    if uuid not in UUID_MAP[username]:
+      next_idx = len(UUID_MAP[username])
+      UUID_MAP[username][uuid] = next_idx
 
   worker_key = '%s-%scores' % (username, cores)
   suffix = UUID_MAP[username][uuid]

worker/games.py
@@ -290,7 +290,7 @@ def run_game(p, remote, result, spsa, spsa_tuning, tc_limit):
         time.sleep(random.randint(failed_updates*failed_updates, 4 * failed_updates*failed_updates))
 
   if datetime.datetime.now() >= end_time:
-    printout(datetime.datetime.now()+' is past end time '+end_time)
+    printout(str(datetime.datetime.now())+' is past end time '+str(end_time))
     kill_process(p)
 
   return { 'task_alive': True }
@@ -339,7 +339,6 @@ def launch_cutechess(cmd, remote, result, spsa_tuning, games_to_play, tc_limit):
 def run_games(worker_info, password, remote, run, task_id):
   task = run['tasks'][task_id]
   result = {
-    'unique_key': worker_info['unique_key'],
     'username': worker_info['username'],
     'password': password,
     'run_id': str(run['_id']),

fishtest/tests/test_rundb.py
@@ -34,17 +34,16 @@ class CreateRunDBTest(unittest.TestCase):
     print(run['tasks'][0])
     self.assertFalse(run['tasks'][0][u'active'])
     run['tasks'][0][u'active'] = True
-    run['tasks'][0][u'worker_info'] = {'username': 'worker1'}
+    run['tasks'][0][u'worker_info'] = {'username': 'worker1', 'unique_key': 'travis'}
     
     print(util.find_run()['args'])
     
   def test_20_update_task(self):
-    worker_id = 'travis'
-    r= rundb.update_task(worker_id, run_id, 0, {'wins': 1, 'losses': 1, 'draws': rundb.chunk_size-3, 'crashes': 0, 'time_losses': 0}, 1000000, '', 'worker2')
+    r= rundb.update_task(run_id, 0, {'wins': 1, 'losses': 1, 'draws': rundb.chunk_size-3, 'crashes': 0, 'time_losses': 0}, 1000000, '', 'worker2')
     self.assertEqual(r, {'task_alive': False})
-    r= rundb.update_task(worker_id, run_id, 0, {'wins': 1, 'losses': 1, 'draws': rundb.chunk_size-3, 'crashes': 0, 'time_losses': 0}, 1000000, '', 'worker1')
+    r= rundb.update_task(run_id, 0, {'wins': 1, 'losses': 1, 'draws': rundb.chunk_size-3, 'crashes': 0, 'time_losses': 0}, 1000000, '', 'worker1')
     self.assertEqual(r, {'task_alive': True})
-    r= rundb.update_task(worker_id, run_id, 0, {'wins': 1, 'losses': 1, 'draws': rundb.chunk_size-2, 'crashes': 0, 'time_losses': 0}, 1000000, '', 'worker1')
+    r= rundb.update_task(run_id, 0, {'wins': 1, 'losses': 1, 'draws': rundb.chunk_size-2, 'crashes': 0, 'time_losses': 0}, 1000000, '', 'worker1')
     self.assertEqual(r, {'task_alive': False})
 
   def test_30_finish(self):