95ee566261926a0132d26e8d8eb0890f1dca4596
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-09-09 07:44:41 +0200

    Remove unused admins group code.
    Fix also multiple appends to this group.

--- fishtest/fishtest/models.py
+++ fishtest/fishtest/models.py
@@ -2,9 +2,8 @@ from pyramid.security import Allow, Everyone
 
 class RootFactory(object):
   __acl__ = [ (Allow, Everyone, 'view'),
-              (Allow, 'group:admins', 'modify_db'),
-              (Allow, 'group:approvers', 'approve_run'),
-              (Allow, 'group:stats', 'modify_stats'), ]
+              (Allow, 'group:approvers', 'approve_run')
+            ]
   def __init__(self, request):
     pass
 
