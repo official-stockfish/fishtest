7db731d8685fee4b3ad5b905683eeae33f4f263a
ppigazzini <pasquale.pigazzini@gmail.com>
2019-11-17 22:43:41 +0100

    Update the backup script for Ubuntu 18.04
    Update the backup script to work with Ubuntu 18.04 and python virtual environment.

--- fishtest/utils/backup.sh
+++ fishtest/utils/backup.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+# Backup MongoDB to AWS S3, download a backup with:
+# ${VENV}/bin/fetch_file s3://fishtest/backup/archive/<YYYYMMDD>/dump.tar.gz -o dump.tar.gz
 
 # Load the variables with the AWS keys, cron uses a limited environment
-source ${HOME}/.bashrc
+. ${HOME}/.profile
 
 cd ${HOME}/backup
-${HOME}/mongodb/bin/mongodump && \
+mongodump && \
 rm -f dump.tar.gz && \
 rm -f dump/fishtest_new/pgns.* && \
 tar -czvf dump.tar.gz dump && \
@@ -13,6 +15,6 @@ rm -rf dump
 date_utc=$(date +%Y%m%d --utc)
 mkdir -p archive/${date_utc}
 mv dump.tar.gz archive/${date_utc}
-s3put -b fishtest -p /home/fishtest/ archive/${date_utc}/dump.tar.gz
-# Keep only the latest archive locally, we've filled the HD up multiple times
+${VENV}/bin/s3put -b fishtest -p ${HOME} archive/${date_utc}/dump.tar.gz
+# Keep only the latest archive locally
 mv archive/${date_utc}/dump.tar.gz .
