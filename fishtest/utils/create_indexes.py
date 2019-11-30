915a1c1b20cf81ce13e43c263e9eb7126f4a6e4a
xoto10 <buylow001@gmail.com>
2019-11-30 17:34:28 +0000

    Script to create indexes

---
+++ fishtest/utils/create_indexes.py
@@ -0,0 +1,74 @@
+
+# create_indexes.py - (re-)create indexes
+#
+# Run this script manually to create the indexes, it could take a few seconds/minutes
+# to run.
+#
+# If any indexes need to be removed, edit dropList as appropriate.
+
+from __future__ import print_function
+
+import os
+import sys
+import pprint
+from pymongo import MongoClient, ASCENDING, DESCENDING
+
+
+db_name='fishtest_new'
+
+# MongoDB server is assumed to be on the same machine, if not user should use
+# ssh with port forwarding to access the remote host.
+conn = MongoClient(os.getenv('FISHTEST_HOST') or 'localhost')
+db = conn[db_name]
+runs = db['runs']
+pgns = db['pgns']
+
+
+indexList = { 'runs' : [ [(u'args.username', ASCENDING)],
+                         [(u'deleted', ASCENDING)],
+                         [(u'finished', ASCENDING), (u'last_updated', DESCENDING)],
+                         [(u'finished', ASCENDING), (u'last_updated', DESCENDING), (u'start_time', DESCENDING)] ],
+                         # [(u'deleted', ASCENDING), (u'finished', ASCENDING)],
+                         # [(u'last_updated', DESCENDING), (u'start_time', DESCENDING)],
+                         # [(u'tasks.pending', ASCENDING)],
+              'pgns' : [ [('run_id', ASCENDING)] ],
+              'users' : [ [('username', ASCENDING)] ]
+            }
+uniqueList = [ 'username' ]
+
+
+def printout(s):
+  print(s)
+  sys.stdout.flush()
+
+
+# Loop through all collections and recreate indexes if given in indexList
+for cn in db.list_collection_names():
+  if cn in indexList:
+    c = db[cn]
+
+    # Drop all indexes on collections cn except _id_
+    printout("")
+    k = c.index_information().keys()
+    k.sort()
+    for idx in k:
+      if idx != "_id_":
+        printout("Dropping " + cn + " index " + idx + " ...")
+        c.drop_index(idx)
+
+    # Create indexes in indexList
+    printout("")
+    for flds in indexList[cn]:
+      printout("Creating " + cn + " index " + str(flds) + " ...")
+      uniq = True if flds[0][0] in uniqueList else False
+      c.create_index(flds, unique=uniq)
+
+
+# Display current list of indexes
+for cn in db.list_collection_names():
+  c = db[cn]
+  printout("\nCurrent indexes on " + cn + ":")
+  pprint.pprint(c.index_information(), stream=None, indent=2, width=110, depth=None)
+
+printout("")
+
