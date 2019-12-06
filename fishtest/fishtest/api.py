b5fee1d63557004ff1b904d916d0cdddcb4f914e
ppigazzini <pasquale.pigazzini@gmail.com>
2019-12-06 10:36:36 +0100

    Raise worker version to 70 (server side)
    Force the workers update in fishtest

--- fishtest/fishtest/api.py
+++ fishtest/fishtest/api.py
@@ -176,7 +176,7 @@ def request_version(request):
   token = authenticate(request)
   if 'error' in token: return json.dumps(token)
 
-  return json.dumps({'version': 69})
+  return json.dumps({'version': 70})
 
 @view_config(route_name='api_request_spsa', renderer='string')
 def request_spsa(request):
