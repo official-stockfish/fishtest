fce3cc794ce8e9045559d512f40f2f7bc0c0b1c8
ppigazzini <pasquale.pigazzini@gmail.com>
2021-11-12 23:10:02 +0100

    Raise worker version to 130 (also server side)

--- worker/worker.py
+++ worker/worker.py
@@ -34,7 +34,7 @@ except ImportError:
 from games import FatalException, WorkerException, run_games, str_signal
 from updater import update
 
-WORKER_VERSION = 129
+WORKER_VERSION = 130
 HTTP_TIMEOUT = 15.0
 MAX_RETRY_TIME = 14400.0  # four hours
 IS_WINDOWS = "windows" in platform.system().lower()
