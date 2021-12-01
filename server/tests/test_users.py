1d229219b9bd8a3f0049b70f22ac4160eda36baa
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2021-12-01 14:41:30 +0100

    Simplify authentication in api.py.
    As a bonus we can now also return "blocked" messages.

--- server/tests/test_users.py
+++ server/tests/test_users.py
@@ -56,12 +56,12 @@ class Create50LoginTest(unittest.TestCase):
             params={"username": "JoeUser", "password": "badsecret"},
         )
         response = login(request)
-        self.assertTrue("Invalid password" in request.session.pop_flash("error"))
+        self.assertTrue("Invalid password for user JoeUser" in request.session.pop_flash("error"))
 
         # Correct password, but still blocked from logging in
         request.params["password"] = "secret"
         login(request)
-        self.assertTrue("Blocked" in request.session.pop_flash("error"))
+        self.assertTrue("User JoeUser is blocked" in request.session.pop_flash("error"))
 
         # Unblock, then user can log in successfully
         user = self.rundb.userdb.get_user("JoeUser")
