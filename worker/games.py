c8ee794f195a5ff8bf2aeeefa5eeb87afc4e4fd2
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2022-02-20 11:12:45 +0100

    Compatibility fix for Python 3.4.

--- worker/games.py
+++ worker/games.py
@@ -100,7 +100,7 @@ def backup_log():
 def str_signal(signal_):
     try:
         return str(signal.Signals(signal_)).split(".")[-1]
-    except ValueError:
+    except (ValueError, AttributeError):
         return "SIG<{}>".format(signal_)
 
 
