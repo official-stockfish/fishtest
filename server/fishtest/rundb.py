official-stockfish/fishtest

commit 5ffb363957cdac985bb57fb3a1c6c3c9424e2f40

Simplify SPSA "clipping" and "rounding" code
- drop the dead (and dubious) "careful clipping" algorithm
- use "stochastic rounding"
https://en.wikipedia.org/wiki/Rounding#Stochastic_rounding
- clean up all references of "clipping" and "rounding"

server/fishtest/rundb.py
@@ -1119,35 +1119,10 @@ class RunDb:
 
         return message
 
-    def spsa_param_clip_round(self, param, increment, clipping, rounding):
-        if clipping == "old":
-            value = param["theta"] + increment
-            if value < param["min"]:
-                value = param["min"]
-            elif value > param["max"]:
-                value = param["max"]
-        else:  # clipping == 'careful':
-            inc = min(
-                abs(increment),
-                abs(param["theta"] - param["min"]) / 2,
-                abs(param["theta"] - param["max"]) / 2,
-            )
-            if inc > 0:
-                value = param["theta"] + inc * increment / abs(increment)
-            else:  # revert to old behavior to bounce off boundary
-                value = param["theta"] + increment
-                if value < param["min"]:
-                    value = param["min"]
-                elif value > param["max"]:
-                    value = param["max"]
-
-        # 'deterministic' rounding calls round() inside the worker.
-        # 'randomized' says 4.p should be 5 with probability p,
-        # 4 with probability 1-p,
-        # and is continuous (albeit after expectation) unlike round().
-        if rounding == "randomized":
-            value = math.floor(value + random.uniform(0, 1))
-
+    def spsa_param_clip_round(self, param, increment):
+        value = min(max(param["theta"] + increment, param["min"]), param["max"])
+        # Stochastic rounding and probability for float N.p: (N, 1-p); (N+1, p)
+        value = math.floor(value + random.uniform(0, 1))
         return value
 
     # Store SPSA parameters for each worker
@@ -1189,10 +1164,6 @@ class RunDb:
     def generate_spsa(self, run):
         result = {"task_alive": True, "w_params": [], "b_params": []}
         spsa = run["args"]["spsa"]
-        if "clipping" not in spsa:
-            spsa["clipping"] = "old"
-        if "rounding" not in spsa:
-            spsa["rounding"] = "deterministic"
 
         # Generate the next set of tuning parameters
         iter_local = spsa["iter"] + 1  # assume at least one completed,
@@ -1203,9 +1174,7 @@ class RunDb:
             result["w_params"].append(
                 {
                     "name": param["name"],
-                    "value": self.spsa_param_clip_round(
-                        param, c * flip, spsa["clipping"], spsa["rounding"]
-                    ),
+                    "value": self.spsa_param_clip_round(param, c * flip),
                     "R": param["a"]
                     / (spsa["A"] + iter_local) ** spsa["alpha"]
                     / c**2,
@@ -1216,9 +1185,7 @@ class RunDb:
             result["b_params"].append(
                 {
                     "name": param["name"],
-                    "value": self.spsa_param_clip_round(
-                        param, -c * flip, spsa["clipping"], spsa["rounding"]
-                    ),
+                    "value": self.spsa_param_clip_round(param, -c * flip),
                 }
             )
 
@@ -1226,9 +1193,6 @@ class RunDb:
 
     def update_spsa(self, worker, run, spsa_results):
         spsa = run["args"]["spsa"]
-        if "clipping" not in spsa:
-            spsa["clipping"] = "old"
-
         spsa["iter"] += int(spsa_results["num_games"] / 2)
 
         # Store the history every 'freq' iterations.
@@ -1250,9 +1214,7 @@ class RunDb:
             R = w_params[idx]["R"]
             c = w_params[idx]["c"]
             flip = w_params[idx]["flip"]
-            param["theta"] = self.spsa_param_clip_round(
-                param, R * c * result * flip, spsa["clipping"], "deterministic"
-            )
+            param["theta"] = self.spsa_param_clip_round(param, R * c * result * flip)
             if grow_summary:
                 summary.append({"theta": param["theta"], "R": R, "c": c})
 

server/fishtest/templates/tests_run.mak
@@ -300,8 +300,6 @@ else:
         </i>
       </div>
     </div>
-    <input type="hidden" name="spsa_clipping" value="old" />
-    <input type="hidden" name="spsa_rounding" value="deterministic" />
   </section>
 
   <section id="worker-and-queue-options">

server/fishtest/views.py
@@ -728,8 +728,6 @@ def validate_form(request):
             "raw_params": request.POST["spsa_raw_params"],
             "iter": 0,
             "num_iter": int(data["num_games"] / 2),
-            "clipping": request.POST["spsa_clipping"],
-            "rounding": request.POST["spsa_rounding"],
         }
         data["spsa"]["params"] = parse_spsa_params(
             request.POST["spsa_raw_params"], data["spsa"]
@@ -1074,13 +1072,11 @@ def tests_view(request):
             A = value["A"]
             alpha = value["alpha"]
             gamma = value["gamma"]
-            summary = "Iter: {:d}, A: {:d}, alpha {:0.3f}, gamma {:0.3f}, clipping {}, rounding {}".format(
+            summary = "Iter: {:d}, A: {:d}, alpha {:0.3f}, gamma {:0.3f}".format(
                 iter_local,
                 A,
                 alpha,
                 gamma,
-                value["clipping"] if "clipping" in value else "old",
-                value["rounding"] if "rounding" in value else "deterministic",
             )
             params = value["params"]
             value = [summary]

commit 1cb0b813a0de9930bf90724eed483ddb44894987

Do not round the SPSA local parameters
Fix a bug introduced with #1306
Move worker side the stochastic rounding of the SPSA parameters.

server/fishtest/rundb.py
@@ -1119,11 +1119,8 @@ class RunDb:
 
         return message
 
-    def spsa_param_clip_round(self, param, increment):
-        value = min(max(param["theta"] + increment, param["min"]), param["max"])
-        # Stochastic rounding and probability for float N.p: (N, 1-p); (N+1, p)
-        value = math.floor(value + random.uniform(0, 1))
-        return value
+    def spsa_param_clip(self, param, increment):
+        return min(max(param["theta"] + increment, param["min"]), param["max"])
 
     # Store SPSA parameters for each worker
     spsa_params = {}
@@ -1174,7 +1171,7 @@ class RunDb:
             result["w_params"].append(
                 {
                     "name": param["name"],
-                    "value": self.spsa_param_clip_round(param, c * flip),
+                    "value": self.spsa_param_clip(param, c * flip),
                     "R": param["a"]
                     / (spsa["A"] + iter_local) ** spsa["alpha"]
                     / c**2,
@@ -1185,7 +1182,7 @@ class RunDb:
             result["b_params"].append(
                 {
                     "name": param["name"],
-                    "value": self.spsa_param_clip_round(param, -c * flip),
+                    "value": self.spsa_param_clip(param, -c * flip),
                 }
             )
 
@@ -1214,7 +1211,7 @@ class RunDb:
             R = w_params[idx]["R"]
             c = w_params[idx]["c"]
             flip = w_params[idx]["flip"]
-            param["theta"] = self.spsa_param_clip_round(param, R * c * result * flip)
+            param["theta"] = self.spsa_param_clip(param, R * c * result * flip)
             if grow_summary:
                 summary.append({"theta": param["theta"], "R": R, "c": c})
 

worker/games.py
@@ -8,6 +8,7 @@ import math
 import multiprocessing
 import os
 import platform
+import random
 import re
 import shutil
 import signal
@@ -996,16 +997,27 @@ def launch_cutechess(
         b_params = []
 
     # Run cutechess-cli binary.
+    # Stochastic rounding and probability for float N.p: (N, 1-p); (N+1, p)
     idx = cmd.index("_spsa_")
     cmd = (
         cmd[:idx]
-        + ["option.{}={}".format(x["name"], round(x["value"])) for x in w_params]
+        + [
+            "option.{}={}".format(
+                x["name"], math.floor(x["value"] + random.uniform(0, 1))
+            )
+            for x in w_params
+        ]
         + cmd[idx + 1 :]
     )
     idx = cmd.index("_spsa_")
     cmd = (
         cmd[:idx]
-        + ["option.{}={}".format(x["name"], round(x["value"])) for x in b_params]
+        + [
+            "option.{}={}".format(
+                x["name"], math.floor(x["value"] + random.uniform(0, 1))
+            )
+            for x in b_params
+        ]
         + cmd[idx + 1 :]
     )
 