3bb9ef450e67df790134ee3dd1666933a003062b
ppigazzini <pasquale.pigazzini@gmail.com>
2022-04-08 11:59:49 +0200

    Raise worker version to 163 (also server side)
    Auto format code with:
    ```
    black --exclude='env|packages' .
    isort --profile black --skip env --skip venv --skip worker/packages .
    ```

--- server/fishtest/api.py
+++ server/fishtest/api.py
@@ -27,7 +27,7 @@ db directly. However this information may be slightly outdated, depending
 on how frequently the main instance flushes its run cache.
 """
 
-WORKER_VERSION = 162
+WORKER_VERSION = 163
 
 flag_cache = {}
 
--- worker/worker.py
+++ worker/worker.py
@@ -46,7 +46,7 @@ from games import (
 )
 from updater import update
 
-WORKER_VERSION = 162
+WORKER_VERSION = 163
 HTTP_TIMEOUT = 30.0
 INITIAL_RETRY_TIME = 15.0
 THREAD_JOIN_TIMEOUT = 15.0
@@ -702,9 +702,13 @@ def get_exception(files):
     i = 0
     exc_type, exc_obj, tb = sys.exc_info()
     filename, lineno, name, line = traceback.extract_tb(tb)[i]
-    message = "Exception {} at {}:{}".format(exc_type.__name__, os.path.basename(filename), lineno)
+    message = "Exception {} at {}:{}".format(
+        exc_type.__name__, os.path.basename(filename), lineno
+    )
     while os.path.basename(filename) in files:
-        message = "Exception {} at {}:{}".format(exc_type.__name__, os.path.basename(filename), lineno)
+        message = "Exception {} at {}:{}".format(
+            exc_type.__name__, os.path.basename(filename), lineno
+        )
         i += 1
         try:
             filename, lineno, name, line = traceback.extract_tb(tb)[i]
