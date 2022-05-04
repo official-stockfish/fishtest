9b68448d02a199b617b6f0dee384b0d6ea1d847f
ppigazzini <pasquale.pigazzini@gmail.com>
2022-05-04 13:58:50 +0200

    Raise worker version to 166 (also server side)

--- server/fishtest/api.py
+++ server/fishtest/api.py
@@ -27,7 +27,7 @@ db directly. However this information may be slightly outdated, depending
 on how frequently the main instance flushes its run cache.
 """
 
-WORKER_VERSION = 165
+WORKER_VERSION = 166
 
 flag_cache = {}
 
