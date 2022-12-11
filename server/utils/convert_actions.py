9e1209135ca6a024e379f40267b9705778b7389c
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2022-12-11 10:23:39 +0100

    PR2: Extract the event fields directly from the data base.
    To make this work the action database should be converted
    using the script utils/convert_actions.py.
    
    Note: this script does not delete data. But still it would be good
    to temporarily keep a backup of the current action data base, just in case
    something goes wrong.
    
    This PR has no user visible changes.

---
+++ server/utils/convert_actions.py
@@ -0,0 +1,33 @@
+import datetime
+
+import pymongo
+from fishtest.actiondb import expand_action
+
+
+if __name__ == "__main__":
+    client = pymongo.MongoClient()
+    actions_collection = client["fishtest_new"]["actions"]
+    actions = actions_collection.find({}).sort("_id", 1)
+    count = 0
+    print("Starting conversion...")
+    t0 = datetime.datetime.utcnow()
+    for action in actions:
+        count += 1
+        action_id = action["_id"]
+        expand_action(action)
+        action["_id"] = action_id
+#        if "data" in action:
+#            del action["data"]
+        actions_collection.replace_one({"_id": action_id}, action)
+        print("Actions converted: {}.".format(count), end="\r")
+    t1 = datetime.datetime.utcnow()
+    duration = (t1 - t0).total_seconds()
+    time_per_run = duration / count
+    print("")
+    print(
+        "Conversion finished in {:.2f} seconds. Time per run: {:.2f}ms.".format(
+            duration, 1000 * time_per_run
+        )
+    )
+    actions.close()
+    client.close()
