70fdf3a8eba95735499653ebb8e87d4b698f1286
FishChess <118218359+FishChess@users.noreply.github.com>
2022-12-18 13:49:35 +0330

    Update toggle_password.js

--- server/fishtest/static/js/toggle_password.js
+++ server/fishtest/static/js/toggle_password.js
@@ -1,14 +1,17 @@
-(() => {
-  "use strict";
+(()=>{
+    "use strict";
 
-  const togglePasswordVisibility = document.querySelectorAll('.toggle-password-visibility');
-  togglePasswordVisibility.forEach(toggle => {
-    toggle.addEventListener('click', event => {
-      const input = event.target.parentNode.querySelector('input');
-      const icon = event.target.querySelector('i');
-      input.type = input.type === 'password' ? 'text' : 'password';
-      icon.classList.toggle('fa-eye');
-      icon.classList.toggle('fa-eye-slash');
-    });
-  });
-})()
+    const togglePasswordVisibility = document.querySelectorAll('.toggle-password-visibility');
+    togglePasswordVisibility.forEach(toggle=>{
+        toggle.addEventListener('click', event=>{
+            const input = event.target.parentNode.querySelector('input');
+            const icon = event.target.querySelector('i');
+            input.type = input.type === 'password' ? 'text' : 'password';
+            icon.classList.toggle('fa-eye');
+            icon.classList.toggle('fa-eye-slash');
+        }
+        );
+    }
+    );
+}
+)()
