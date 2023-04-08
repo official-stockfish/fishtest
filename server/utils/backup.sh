eef97ecc849bc9b67ad6c232e52e676ba0e85c6d
Maxim Masiutin <maxim@masiutin.com>
2023-04-08 07:38:05 +0300

    Dump first all files to one tar and then use multithreaded gzip (pigz) with fastest compression to save CPU as much as possible, then move the file to aws instead of copying it

--- server/utils/backup.sh
+++ server/utils/backup.sh
@@ -4,18 +4,18 @@
 # List available backups with:
 # ${VENV}/bin/aws s3 ls s3://fishtest/backup/archive/
 # Download a backup with:
-# ${VENV}/bin/aws s3 cp s3://fishtest/backup/archive/<YYYYMMDD>/dump.tar dump.tar
+# ${VENV}/bin/aws s3 cp s3://fishtest/backup/archive/<YYYYMMDD>/dump.tar.gz dump.tar.gz
 
 # Load the variables with the AWS keys, cron uses a limited environment
 . ${HOME}/.profile
 
 cd ${HOME}/backup
 for db_name in "fishtest_new" "admin" "fishtest" "fishtest_testing"; do
-  mongodump --db=${db_name} --numParallelCollections=1 --excludeCollection="pgns" --gzip
+  mongodump --db=${db_name} --numParallelCollections=1 --excludeCollection="pgns"
 done
 tar -cvf dump.tar dump
 rm -rf dump
-
+pigz --fast dump.tar
 date_utc=$(date +%Y%m%d --utc)
 ${VENV}/bin/aws configure set default.s3.max_concurrent_requests 1
-${VENV}/bin/aws s3 cp dump.tar s3://fishtest/backup/archive/${date_utc}/dump.tar
+${VENV}/bin/aws s3 mv dump.tar.gz s3://fishtest/backup/archive/${date_utc}/dump.tar.gz
