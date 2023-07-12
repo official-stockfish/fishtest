3f194ff919b1217cd3a008febf62291bc6003b53
ppigazzini <pasquale.pigazzini@gmail.com>
2023-07-12 16:03:48 +0200

    Raise worker version to 208

--- server/fishtest/api.py
+++ server/fishtest/api.py
@@ -25,7 +25,7 @@ db directly. However this information may be slightly outdated, depending
 on how frequently the main instance flushes its run cache.
 """
 
-WORKER_VERSION = 207
+WORKER_VERSION = 208
 
 
 def validate_request(request):
