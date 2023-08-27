5410bb0f02059424d056570b5a8f020d6be455c8
ppigazzini <pasquale.pigazzini@gmail.com>
2023-08-27 23:49:28 +0200

    Replace deprecated `distutils` package
    The package `distutils` is deprecated since python 3.10
    and it is scheduled to be removed with python 3.12, see:
    https://docs.python.org/3.12/whatsnew/3.12.html
    https://github.com/python/cpython/issues/92584
    
    `shutil.copytree()` is able to copy the source directory tree to an existing
    destination directory only from python 3.8 (parameter `dirs_exist_ok=True`),
    for python < 3.8 still use `distutils.dir_util.copy_tree()`
    
    While here:
    - create in a safe way the temporary directory used for the update
    - deal with possible download problems
    
    Raise worker version to 214 (also server side)

--- worker/updater.py
+++ worker/updater.py
@@ -1,8 +1,8 @@
 import os
 import shutil
 import sys
+import tempfile
 from datetime import datetime, timezone
-from distutils.dir_util import copy_tree
 from pathlib import Path
 from zipfile import ZipFile
 
@@ -26,12 +26,24 @@ def do_restart():
 
 def update(restart=True, test=False):
     worker_dir = Path(__file__).resolve().parent
-    update_dir = worker_dir / "update"
-    update_dir.mkdir(exist_ok=True)
-
+    update_dir = Path(tempfile.mkdtemp(dir=worker_dir))
     worker_zip = update_dir / "wk.zip"
-    with open(worker_zip, "wb+") as f:
-        f.write(requests.get(WORKER_URL).content)
+
+    try:
+        response = requests.get(WORKER_URL)
+        response.raise_for_status()
+    except Exception as e:
+        print(
+            "Failed to download {}:\n".format(WORKER_URL),
+            e,
+            sep="",
+            file=sys.stderr,
+        )
+        shutil.rmtree(update_dir)
+        return None
+    else:
+        with open(worker_zip, "wb+") as f:
+            f.write(response.content)
 
     with ZipFile(worker_zip) as zip_file:
         zip_file.extractall(update_dir)
@@ -57,7 +69,13 @@ def update(restart=True, test=False):
                     sep="",
                     file=sys.stderr,
                 )
-        copy_tree(str(worker_src), str(worker_dir))
+        if sys.version_info < (3, 8):
+            from distutils.dir_util import copy_tree
+
+            copy_tree(str(worker_src), str(worker_dir))
+        else:
+            shutil.copytree(worker_src, worker_dir, dirs_exist_ok=True)
+
     else:
         file_list = os.listdir(worker_src)
     shutil.rmtree(update_dir)
