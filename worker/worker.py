c5e41649b13f64dbdc8041f7d566af01c1bc3e7f
Robert Nurnberg @ elitebook <robert.nurnberg@gmx.de>
2023-08-31 15:02:17 +0200

    Send full commit SHAs to cutechess-cli
    Raise worker version to 215 (also server side)

--- worker/worker.py
+++ worker/worker.py
@@ -54,7 +54,7 @@ from updater import update
 # Several packages are called "expression".
 # So we make sure to use the locally installed one.
 
-WORKER_VERSION = 214
+WORKER_VERSION = 215
 FILE_LIST = ["updater.py", "worker.py", "games.py"]
 HTTP_TIMEOUT = 30.0
 INITIAL_RETRY_TIME = 15.0
