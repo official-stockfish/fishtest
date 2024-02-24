b274f2e19f783a0c4472b5ac3d375d33bacc82e6
ppigazzini <pasquale.pigazzini@gmail.com>
2024-02-25 00:32:41 +0100

    Set the reference nps for sf_16.1 wrt sf_11
    Fishtest with Stockfish 11 (#635) had 1.6 Mnps as reference core nps
    and 0.7 Mnps as threshold nps to stop a slow worker.
    Set the new reference core nps to 691680 nps according to the 56.77% slowdown
    of sf_16.1 wrt sf_11 on the ARCH=x86-64-bmi2 of the Ryzen 7 PRO 7840U.
    Set the new threshold for slow worker to 208082 nps according to the 70.27%
    slowdown of sf_16.1 wrt sf_11 on the ARCH=x86-64-sse41-popcnt of the
    Core i7 3770K.
    
    Stockfish compiled with clang++ 18.1.0
    Parallel speedup with bench at depth 13
    
    - ARCH=x86-64-bmi2 (AMD Ryzen 7 PRO 7840U)
    ```
    sf_base =  3451831 +/-   6037 (95%)
    sf_test =  1492232 +/-   3012 (95%)
    diff    = -1959598 +/-   4685 (95%)
    speedup = -56.770% +/- 0.136% (95%)
    ```
    - ARCH=x86-84-bmi2 (Intel Xeon E5-2680 v3)
    ```
    sf_base =  1918881 +/-   1002 (95%)
    sf_test =   649033 +/-    675 (95%)
    diff    = -1269847 +/-   1154 (95%)
    speedup = -66.176% +/- 0.060% (95%)
    ```
    - ARCH=x86-64-sse41-popcnt (Intel Core i7 3770K)
    ```
    sf_base =  2099079 +/-   9345 (95%)
    sf_test =   623973 +/-   2121 (95%)
    diff    = -1475105 +/-   7601 (95%)
    speedup = -70.274% +/- 0.362% (95%)
    ```
    
    Raise worker version to 230 (also server side)

--- server/utils/delta_update_users.py
+++ server/utils/delta_update_users.py
@@ -45,10 +45,10 @@ def initialize_info(rundb, clear_stats):
 
 
 def compute_games_rates(rundb, info_tuple):
-    # 1184000 nps is the reference core, also set in rundb.py and games.py
+    # use the reference core nps, also set in rundb.py and games.py
     for machine in rundb.get_machines():
         games_per_hour = (
-            (machine["nps"] / 640000)
+            (machine["nps"] / 691680)
             * (3600.0 / estimate_game_duration(machine["run"]["args"]["tc"]))
             * (int(machine["concurrency"]) // machine["run"]["args"].get("threads", 1))
         )
