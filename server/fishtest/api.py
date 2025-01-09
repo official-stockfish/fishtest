c7f775e357057ef70f527ab792152057755cb729
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2025-01-09 17:49:11 +0100

    Make fish.exit quit the worker after the next update_task.
    In other words: do not wait for the whole task to finish.
    
    Raise worker version to 250 (also server side).

--- server/fishtest/api.py
+++ server/fishtest/api.py
@@ -34,7 +34,7 @@ Proper configuration of `nginx` is crucial for this, and should be done
 according to the route/URL mapping defined in `__init__.py`.
 """
 
-WORKER_VERSION = 249
+WORKER_VERSION = 250
 
 
 @exception_view_config(HTTPException)
