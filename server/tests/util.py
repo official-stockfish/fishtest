26cacdebf0fd0402de86accd413bde9d0c66a5d6
ppigazzini <pasquale.pigazzini@gmail.com>
2025-02-10 14:09:03 +0100

    fix: close test mongodb connection on exit

--- server/tests/util.py
+++ server/tests/util.py
@@ -1,12 +1,17 @@
+import atexit
+
 from fishtest.rundb import RunDb
 
 
 def get_rundb():
-    return RunDb(db_name="fishtest_tests")
+    rundb = RunDb(db_name="fishtest_tests")
+    atexit.register(rundb.conn.close)
+    return rundb
 
 
 def find_run(arg="username", value="travis"):
     rundb = RunDb(db_name="fishtest_tests")
+    atexit.register(rundb.conn.close)
     for run in rundb.get_unfinished_runs():
         if run["args"][arg] == value:
             return run
