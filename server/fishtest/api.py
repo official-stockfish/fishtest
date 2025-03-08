11be66643d4b89fed96f554f162c17f03bbb461d
ppigazzini <pasquale.pigazzini@gmail.com>
2025-03-08 22:28:17 +0100

    feat(worker): bench setting the hash
    Raise worker version to 268 (also server side)

--- server/fishtest/api.py
+++ server/fishtest/api.py
@@ -35,7 +35,7 @@ Proper configuration of `nginx` is crucial for this, and should be done
 according to the route/URL mapping defined in `__init__.py`.
 """
 
-WORKER_VERSION = 267
+WORKER_VERSION = 268
 
 
 @exception_view_config(HTTPException)
