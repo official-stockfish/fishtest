6e926338b1cb0213de570b849fb2c7ac68eb9204
Robert Nurnberg @ elitebook <robert.nurnberg@gmx.de>
2025-11-22 18:11:22 +0100

    [WIP] update fastchess

--- server/fishtest/api.py
+++ server/fishtest/api.py
@@ -36,7 +36,7 @@ Proper configuration of `nginx` is crucial for this, and should be done
 according to the route/URL mapping defined in `__init__.py`.
 """
 
-WORKER_VERSION = 302
+WORKER_VERSION = 303
 
 
 @exception_view_config(HTTPException)
