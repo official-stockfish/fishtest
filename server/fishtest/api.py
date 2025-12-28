815a3ed418df388d48d4d54fdedd2e7cf3c9634d
Disservin <disservin.social@gmail.com>
2025-12-28 20:42:36 +0100

    update worker

--- server/fishtest/api.py
+++ server/fishtest/api.py
@@ -37,7 +37,7 @@ Proper configuration of `nginx` is crucial for this, and should be done
 according to the route/URL mapping defined in `__init__.py`.
 """
 
-WORKER_VERSION = 305
+WORKER_VERSION = 306
 
 
 @exception_view_config(HTTPException)
