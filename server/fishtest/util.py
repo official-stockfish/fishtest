23f75d073ebd7ef16cd23ac650f23f6ead9e8162
ppigazzini <pasquale.pigazzini@gmail.com>
2026-02-06 16:14:22 +0100

    feat(server): update CPU architectures list
    Uncomment newly supported ARCH values detected by `get_native_properties.sh` script.
    See https://github.com/official-stockfish/Stockfish/commit/3ee16a1d8e96a095fcb1c96653031c9758cefc06

--- server/fishtest/util.py
+++ server/fishtest/util.py
@@ -641,13 +641,11 @@ def reasonable_run_hashes(run):
 supported_compilers = ["clang++", "g++"]
 
 
-# The list below has been extracted from the output of "make help"
-# in the src directory of the SF source code.
-# However not all arches supported by the makefile are actually
-# used by the workers since they use "make ARCH=native" for compilation
-# and this means the arch is determined by the output of
-# script/get_native_properties.sh in the SF source code.
-# The unused arches have been commented out.
+# List of architectures extracted from the output of "make help"
+# in the src directory of the Stockfish source code.
+# The worker compiles with "make ARCH=native", which uses
+# the output of Stockfish script "get_native_properties.sh".
+# Architectures not covered by the script must be listed as commented out.
 
 supported_arches = [
     "apple-silicon",
@@ -655,28 +653,28 @@ supported_arches = [
     "armv7-neon",
     "armv8",
     "armv8-dotprod",
-    #    "e2k",
-    #    "general-32",
-    #    "general-64",
+    "e2k",
+    "general-32",
+    "general-64",
     "loongarch64",
     "loongarch64-lasx",
     "loongarch64-lsx",
-    #    "ppc-32",
+    "ppc-32",
     "ppc-64",
     "ppc-64-altivec",
     "ppc-64-vsx",
-    #    "riscv64",
+    "riscv64",
     "x86-32",
-    #    "x86-32-sse2",
-    #    "x86-32-sse41-popcnt",
+    "x86-32-sse2",
+    "x86-32-sse41-popcnt",
     "x86-64",
     "x86-64-avx2",
     "x86-64-avx512",
     "x86-64-avxvnni",
     "x86-64-bmi2",
-    #    "x86-64-sse3-popcnt",
+    "x86-64-sse3-popcnt",
     "x86-64-sse41-popcnt",
-    #    "x86-64-ssse3",
+    "x86-64-ssse3",
     "x86-64-vnni512",
     "x86-64-avx512icl",
 ]
