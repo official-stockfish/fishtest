279606964343b10bf448b9761735cd8addcfa2ce
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2026-09-05 10:52:14 +0000

    Remove resign adjudication from worker
    Play games with decisive scores until mate, should facilitate retaining good performance to find mate, and provide better testing for the mating capabilities.

--- worker/games.py
+++ worker/games.py
@@ -1799,9 +1799,6 @@ def run_games(
             + ["-srand", f"{run_seed}"]
             + (
                 [
-                    "-resign",
-                    "movecount=3",
-                    "score=600",
                     "-draw",
                     "movenumber=34",
                     "movecount=8",
